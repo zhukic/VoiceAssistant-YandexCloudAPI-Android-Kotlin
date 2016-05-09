b4b50033d52dfb1637fc9882f678720d5833ed51
zhukic <zhukic@mail.ru>
2016-05-09 20:46:57 +0300

    add widget

--- app/src/main/java/rus/voiceassistant/Main2Activity.java
+++ app/src/main/java/rus/voiceassistant/Main2Activity.java
@@ -30,6 +30,7 @@ public class Main2Activity extends Activity {
      * Showing google speech input dialog
      * */
     private void promptSpeechInput() {
+
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
